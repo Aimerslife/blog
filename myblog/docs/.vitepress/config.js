20998790cea9ba5d4e03363d25275c237af10fef
10074 <1007458948@qq.com>
2022-07-02 17:34:25 +0800

    1
    1

---
+++ myblog/docs/.vitepress/config.js
@@ -0,0 +1,26 @@
+module.exports={
+    title:'Aimers的博客',
+    description:'About study.',
+    base:'/',
+    head:[
+
+    ],
+    themeConfig:{
+        nav:[
+            {
+                text: "Home",
+                link:'/'
+            },
+        ],
+        sidebar:[
+            {
+                text:"Tags",
+                link:"/tags",
+            },
+            {
+                text:"Archives",
+                link: "/archives",
+            },
+        ]
+    }
+}
